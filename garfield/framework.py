de74e95ead13012749bc205300b9e43b31ac5e8e
Malthe Raschke Nielsen <malthera@pcbe17366.dyndns.cern.ch>
2024-11-19 17:23:54 +0100

    moved generate_Initial_Parameters function from genetic_algorithm into framework.

--- garfield/framework.py
+++ garfield/framework.py
@@ -4,48 +4,15 @@
 Created on Mon Mar 23 13:20:11 2020
 
 @author: sjoly
+@modified by: MaltheRaschke
 """
 import numpy as np
+from functools import partial
 from scipy.optimize import minimize
 
-from .genetic_algorithm import *
-
-
-'''def generate_parameterBounds(pars, margin=0.1):
-    """
-    Function used to regenerate parameter bounds when using the minimization algorithm.
-    Margin argument can be given to allow each parameter to deviate by 100*margin [%].
-    """
-    pars_reshape = pars.reshape(-1, 3)
-    new_bounds = []
-    for par in pars_reshape:
-        new_bounds.extend(tuple(sorted(((1-margin)*p, (1+margin)*p))) for p in par)
-    return new_bounds'''
-
-def display_resonator_parameters(solution):
-    """
-    Displays resonance parameters in a formatted table using ASCII characters.
-
-    Args:
-        solution: A NumPy array of resonator parameters, typically shaped (n_resonators, 3).
-    """
-
-    n_resonators, _ = solution.reshape(-1,3).shape
-    header_format = "{:^10}|{:^24}|{:^18}|{:^18}"
-    data_format = "{:^10d}|{:^24.2e}|{:^18.2f}|{:^18.3e}"
-
-    print("\n")
-    print("-" * 70)
-
-    # Print header
-    print(header_format.format("Resonator", "Rs [Ohm/m or Ohm]", "Q", "fres [Hz]"))
-    print("-" * 70)
-
-    # Print data
-    for i, parameters in enumerate(solution.reshape(-1,3)):
-        print(data_format.format(i + 1, *parameters))
-
-    print("-" * 70)
+from solvers import Solvers
+from objectiveFunctions import ObjectiveFunctions as obj
+from garfield.resonatorFormulas import Impedances as imp
 
 class GeneticAlgorithm:
     def __init__(self, 
@@ -55,8 +22,9 @@ class GeneticAlgorithm:
                  wake_data, 
                  N_resonators, 
                  parameterBounds,
-                 minimizationFunction, 
-                 fitFunction
+                 plane="longitudinal", 
+                 objectiveFunction = obj.sumOfSquaredError, 
+                 wake_length=None,
                 ):
     
         self.frequency_data = frequency_data
@@ -65,9 +33,19 @@ class GeneticAlgorithm:
         self.wake_data = wake_data
         self.N_resonators = N_resonators
         self.parameterBounds = parameterBounds
-        self.minimizationFunction = minimizationFunction
-        self.fitFunction = fitFunction
-        
+        self.objectiveFunction = objectiveFunction
+        self.wake_length = wake_length
+        self.plane = plane
+
+        if plane == "longitudinal" and N_resonators > 1:
+            self.fitFunction = partial(imp.n_Resonator_longitudinal_imp, wake_length=wake_length)
+        elif plane == "transverse" and N_resonators > 1:
+            self.fitFunction = partial(imp.n_Resonator_transverse_imp, wake_length=wake_length)
+        elif plane == "longitudinal" and N_resonators == 1:
+            self.fitFunction = partial(imp.Resonator_longitudinal_imp, wake_length=wake_length)
+        elif plane == "transverse" and N_resonators == 1:
+            self.fitFunction = partial(imp.Resonator_transverse_imp, wake_length=wake_length)
+
         self.geneticParameters = None
         self.minimizationParameters = None
                 
@@ -79,6 +57,75 @@ class GeneticAlgorithm:
         self.frequency_data = self.frequency_data[mask]
         self.impedance_data = self.impedance_data[mask]
     
+
+    def generate_Initial_Parameters(self, parameterBounds, objectiveFunction, fitFunction,
+                                x_values_data, y_values_data,
+                                maxiter=2000, popsize=150, 
+                                mutation=(0.1, 0.5), crossover_rate=0.8,
+                                tol=0.01,
+                                solver='scipy',
+                               ):    
+        """
+        Generates initial parameter guesses for a minimization algorithm.
+
+        This function uses a differential evolution (DE) solver to find approximate
+        solutions to a minimization problem, which can be used as initial guesses for
+        more precise optimizers.
+
+        Args:
+            parameterBounds: A list of tuples representing the upper and lower bounds
+                            for each parameter.
+            objective_function: A function that calculates the cost given a set of
+                            parameters and data. The signature should be
+                            `objective_function(parameters, fit_function, x_data, y_data)`.
+            fit_function: A function that calculates the fit between a model and data.
+                        The signature should be `fit_function(parameters, x_data, y_data)`.
+            x_values_data: The x-values of the data to fit.
+            y_values_data: The y-values of the data to fit.
+            maxiter: The maximum number of iterations for the DE solver.
+            popsize: The population size for the DE algorithm.
+            mutation: A tuple of two floats representing the mutation factors.
+            crossover_rate: The crossover rate for the DE algorithm.
+            tol: The tolerance for convergence.
+            solver: The solver to use for differential evolution. Valid options are
+                    "scipy", "pyfde", or "pyfde_jade". Defaults to "scipy".
+
+        Returns:
+            A tuple containing:
+                - The estimated initial parameters found by the DE solver.
+                - A message indicating the solver's status.
+        """
+
+        
+        objective_function = partial(objectiveFunction, 
+                                        fitFunction=fitFunction,
+                                        x=x_values_data, 
+                                        y=y_values_data
+                                    )
+        
+        # Map solver names to functions
+        solver_functions = {
+            "scipy": Solvers.run_scipy_solver,
+            "pyfde": Solvers.run_pyfde_solver,
+            "pyfde_jade": Solvers.run_pyfde_jade_solver,
+        }
+
+        solver_function = solver_functions.get(solver)
+        if solver == "pyfde_jade":
+            mutation, crossover_rate = None, None
+        
+        if not solver_function:
+            raise ValueError(f"Invalid solver name: {solver}")
+            
+        solution, message = solver_function(parameterBounds, 
+                                            objective_function,
+                                            maxiter=maxiter, 
+                                            popsize=popsize, 
+                                            mutation=mutation, 
+                                            crossover_rate=crossover_rate,
+                                            tol=tol)
+        
+        return solution, message
     
     def run_geneticAlgorithm(self, 
                              maxiter=2000, 
@@ -90,8 +137,8 @@ class GeneticAlgorithm:
                              vectorized=False,
                              solver='scipy',
                              iteration_convergence=False, debug=False):
-        geneticParameters, warning = generate_Initial_Parameters(self.parameterBounds, 
-                                                           self.minimizationFunction, 
+        geneticParameters, warning = self.generate_Initial_Parameters(self.parameterBounds, 
+                                                           self.objectiveFunction, 
                                                            self.fitFunction, 
                                                            self.frequency_data, 
                                                            self.impedance_data, 
@@ -107,7 +154,7 @@ class GeneticAlgorithm:
 
         self.geneticParameters = geneticParameters
         self.warning = warning
-        display_resonator_parameters(self.geneticParameters)
+        self.display_resonator_parameters(self.geneticParameters)
             
     def run_minimizationAlgorithm(self, margin=0.1, method='Nelder-Mead'):
         """
@@ -116,12 +163,12 @@ class GeneticAlgorithm:
         increased or decreased by 100*margin [%].
         """
         print('Method for minimization : '+method)
-        minimization_function = partial(self.minimizationFunction, fitFunction=self.fitFunction,
+        objective_function = partial(self.objectiveFunction, fitFunction=self.fitFunction,
                                 x=self.frequency_data, y=self.impedance_data)
         
         if self.geneticParameters is not None:
             minimizationBounds = [sorted(((1-margin)*p, (1+margin)*p)) for p in self.geneticParameters]
-            minimizationParameters = minimize(minimization_function, 
+            minimizationParameters = minimize(objective_function, 
                                               x0=self.geneticParameters,
                                               bounds=minimizationBounds,
                                               tol=1, #empiric value, documentation is cryptic
@@ -133,7 +180,7 @@ class GeneticAlgorithm:
                                              )
         else:
             print('Genetic algorithm not run, minimization only')
-            minimizationParameters = minimize(minimization_function, 
+            minimizationParameters = minimize(objective_function, 
                                               x0=np.mean(self.parameterBounds, axis=1),
                                               bounds=self.parameterBounds, 
                                               method=method, 
@@ -144,4 +191,34 @@ class GeneticAlgorithm:
                                                        'adaptive': True}
                                              ) 
         self.minimizationParameters = minimizationParameters.x
-        display_resonator_parameters(self.minimizationParameters)
+        self.display_resonator_parameters(self.minimizationParameters)
+    
+    def display_resonator_parameters(self, solution, to_markdown=False):
+        """
+        Displays resonance parameters in a formatted table using ASCII characters.
+
+        Args:
+            solution: A NumPy array of resonator parameters, typically shaped (n_resonators, 3).
+        """
+
+        n_resonators, _ = solution.reshape(-1,3).shape
+        header_format = "{:^10}|{:^24}|{:^18}|{:^18}"
+        data_format = "{:^10d}|{:^24.2e}|{:^18.2f}|{:^18.3e}"
+        if to_markdown:
+            #change format to markdown
+            pass
+        else:
+            print("\n")
+            print("-" * 70)
+
+
+            # Print header
+            print(header_format.format("Resonator", "Rs [Ohm/m or Ohm]", "Q", "fres [Hz]"))
+            print("-" * 70)
+
+            # Print data
+            for i, parameters in enumerate(solution.reshape(-1,3)):
+                print(data_format.format(i + 1, *parameters))
+
+            print("-" * 70)
+
