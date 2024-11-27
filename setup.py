02bd123a6c0794e3295c24f6d3a10044f4ece83b
Malthe Raschke Nielsen <malthera@pcbe17366.dyndns.cern.ch>
2024-11-27 15:55:01 +0100

    Name changes for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 # read version
-version_file = Path(__file__).parent / 'garfield/_version.py'
+version_file = Path(__file__).parent / 'iddefix/_version.py'
 dd = {}
 with open(version_file.absolute(), 'r') as fp:
     exec(fp.read(), dd)
@@ -16,22 +16,22 @@ with open('requirements.txt') as f:
     notebook_required = f.read().splitlines()
 
 setup(
-    name='garfield',
+    name='iddefix',
     version=__version__,
     description="Genetic Algorithm Resonator Fitting for Impedance ExtrapoLation and Determination",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/SebastienJoly/GARFIELD',
+    url='https://github.com/SebastienJoly/IDDEFIX',
     author='Sebastien Joly',
     author_email="sebastien.joly@helmholtz-berlin.de", 
     maintainer='Elena de la Fuente',
     maintainer_email="elena.de.la.fuente.garcia@cern.ch",
     license='GNU GENERAL PUBLIC LICENSE',
-    download_url="https://pypi.python.org/pypi/garfieldx",
+    download_url="https://pypi.python.org/pypi/iddefix",
     project_urls={
-            "Bug Tracker": "https://github.com/ImpedanCEI/GARFIELD/issues",
-            "Documentation": "https://garfield.readthedocs.io/en/latest/.html",
-            "Source Code": "https://github.com/ImpedanCEI/GARFIELD/",
+            "Bug Tracker": "https://github.com/ImpedanCEI/IDDEFIX/issues",
+            "Documentation": "https://iddefix.readthedocs.io/en/latest/.html",
+            "Source Code": "https://github.com/ImpedanCEI/IDDEFIX/",
         },
     packages=find_packages(),
     include_package_data=True,
