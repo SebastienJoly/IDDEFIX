66cb727f6abbcc9cc213e341d97b35988064b632
Malthe Raschke Nielsen <malthera@pcbe17366.dyndns.cern.ch>
2025-01-21 15:46:23 +0100

    Edit of init to include SBD

--- iddefix/__init__.py
+++ iddefix/__init__.py
@@ -2,9 +2,11 @@ from . import framework
 from . import objectiveFunctions
 from . import resonatorFormulas
 from . import solvers
+from . import smartBoundDetermination
 
 from .framework import EvolutionaryAlgorithm
 from .objectiveFunctions import ObjectiveFunctions
 from .resonatorFormulas import Wakes, Impedances
+from .smartBoundDetermination import SmartBoundDetermination
 
 from ._version import __version__
